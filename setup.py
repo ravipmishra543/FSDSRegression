855cacc36cedcea52934c33124042b0cf07dc42c
ravipmishra543 <ravipmishra543@gmail.com>
2023-04-02 17:58:33 +0530

    first commit

--- setup.py
+++ setup.py
@@ -1,4 +1,4 @@
-from setuptools import find_packages, setup
+from setuptools import find_packages,setup
 from typing import List
 
 HYPEN_E_DOT='-e .'
@@ -7,17 +7,19 @@ def get_requirements(file_path:str)->List[str]:
     requirements=[]
     with open(file_path) as file_obj:
         requirements=file_obj.readlines()
-        requirements[req.replace("\n","") for req in requirements]
+        requirements=[req.replace("\n","") for req in requirements]
 
-        if HYPEN_E_DOT in Requirements:
+        if HYPEN_E_DOT in requirements:
             requirements.remove(HYPEN_E_DOT)
+
     return requirements
+
 
 setup(
     name='RegressorProject',
     version='0.0.1',
     author='Ravi Prakash Mishra',
     author_email='ravipmishra543@gmail.com',
-    install_requires=get_requirements('requirements.txt')
+    install_requires=get_requirements('requirements.txt'),
     packages=find_packages()
 )
